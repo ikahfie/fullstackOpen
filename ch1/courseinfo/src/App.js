3a48a9e12f6fdacc144a2332980497cf8b338397
ikahfie <ibrahimkahfie@gmail.com>
2022-09-20 13:56:58 +0700

    commit 1.1

---
+++ ch1/courseinfo/src/App.js
@@ -0,0 +1,50 @@
+const Header = (props) => {
+  return (
+    <h1> {props.course} </h1>
+  )
+}
+
+const Content = (props) => {
+  return(
+    <div>
+      <p>
+        {props.part[0]} {props.exercise[0]}
+      </p>
+      <p>
+        {props.part[1]} {props.exercise[1]}
+      </p>
+      <p>
+        {props.part[2]} {props.exercise[2]}
+      </p>
+    </div>
+  )
+}
+
+const Total = (props) => {
+  return(
+    <p>Number of exercises {props.total} </p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  const part = [part1, part2, part3]
+  const exercise = [exercises1, exercises2, exercises3]
+
+  return (
+    <div>
+      <Header course={course}/>
+      <Content part={part} exercise={exercise}/>
+      <Total total={exercises1+exercises2+exercises3}/>
+    </div>
+  )
+}
+
+export default App
