8e55e1961c49cf81028871b13c2a254dd923624d
ikahfie <ibrahimkahfie@gmail.com>
2022-09-20 14:26:18 +0700

    commit 1.3

--- ch1/courseinfo/src/App.js
+++ ch1/courseinfo/src/App.js
@@ -22,27 +22,38 @@ const Content = (props) => {
 
 const Total = (props) => {
   return(
-    <p>Number of exercises {props.total} </p>
+    <p>
+      Number of exercises {
+        props.exercise[0]+props.exercise[1]+props.exercise[2]
+      }
+    </p>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises : 14
+  }
 
-  const part = [part1, part2, part3]
-  const exercise = [exercises1, exercises2, exercises3]
+  const part = [part1.name, part2.name, part3.name]
+  const exercise = [part1.exercises, part2.exercises, part2.exercises]
 
   return (
     <div>
       <Header course={course}/>
       <Content part={part} exercise={exercise}/>
-      <Total total={exercises1+exercises2+exercises3}/>
+      <Total exercise={exercise}/>
     </div>
   )
 }
